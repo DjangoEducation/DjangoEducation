3a9188d7dc240207697c134cddccab04308f7615
Moataz024 <prexzcod@gmail.com>
2024-10-30 21:00:45 +0100

    Added quiz Manual CRUD and AI Auto Quiz Generation with score counting.

--- firstProject/urls.py
+++ firstProject/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('gestionCours/', include('gestionCours.urls')),
+    path('quizzes/', include('gestionQuiz.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
